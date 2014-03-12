62d83dfd67ce6aa6cba3b24c78ecfe0e229871df
Gilberto DM <gilbertodemelo@Gilbertos-MacBook-Pro.local>
2014-03-11 22:19:26 -0300

    added folder

---
+++ csci3130projectJavaFiles/src/java/Building.java
@@ -0,0 +1,32 @@
+/**
+ * Building Class for Generic Game #1067
+ */
+public class Building {
+    
+    /** attributes */
+    private String name;
+    private Location location;
+    
+    /** constructor */
+    public Building(String name){
+        this.name = name;
+    }
+    
+    /** set methods */
+    public void setBuilding(String name){
+        this.name = name;
+    }
+    
+    /** get methods */
+    public String getBuildingName(){
+        return name;
+    }
+    
+    /** 
+     * Method Name: buildingLocation
+     * Parameters: name
+     * Return: Building location coordinates (latitude, longitude)
+     */
+    
+    
+} // end of class
---
+++ csci3130projectJavaFiles/src/java/Menu.java
@@ -0,0 +1,19 @@
+
+
+
+
+public class Menu {
+    
+    public void profile(){
+        
+    }
+    
+    public void friends(){
+        
+    }
+    
+    public void exit(){
+        
+    }
+    
+} //end of class
